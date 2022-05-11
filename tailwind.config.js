f742c727f7724a9cc2148cca050e7e247d95d9b1
Sergio Vazquez II <sergio@vascos.com>
2022-05-11 07:11:49 -0500

    Add Tailwind CSS v3.0.24

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	content: [
+		'./src/**/*.{html,php,js,jsx,ts,tsx}',
+		'./src/*.{html,php,js,jsx,ts,tsx}',
+		'./app/**/*.{html,php,js,jsx,ts,tsx}',
+		'./app/*.{html,php,js,jsx,ts,tsx}'
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
